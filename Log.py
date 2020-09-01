ec15609842aa93918f680bc638c24d9f29847e18
Mohammadreza Sarayloo <signor.rayan@gmail.com>
2020-09-01 18:12:27 +0430

    Update Log.py

--- Log.py
+++ Log.py
@@ -2,6 +2,10 @@ import logging
 
 
 def set_log(msg, file_name='log.txt'):
-    logging.basicConfig(filename=file_name, level=logging.INFO,
-                        format='%(asctime)s %(filename)s[line:%(lineno)d] %(message)s', datefmt='%Y-%m-%d %X')
+    logging.basicConfig(
+                        filename=file_name,
+                        level=logging.INFO,
+                        format='%(asctime)s %(filename)s[line:%(lineno)d] %(message)s',
+                        datefmt='%Y-%m-%d %X'
+                       )
     logging.info(msg)
